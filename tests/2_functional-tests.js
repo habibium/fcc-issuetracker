6bc37e7d0ee6412aff5e4c96a1d0f6e6abd91f6f
habibium <habib.prodev@gmail.com>
2024-10-19 17:12:16 +0600

    test: create test for viewing issues

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,14 +5,29 @@ const server = require("../server");
 
 chai.use(chaiHttp);
 
+const ISSUE_FIELDS = [
+  "_id",
+  "assigned_to",
+  "issue_title",
+  "issue_title",
+  "created_on",
+  "created_by",
+  "updated_on",
+  "open",
+];
+
+const request = () => chai.request(server).keepOpen();
+
 suite("Functional Tests", function () {
   suite("Create issues", () => {
     test("Create an issue with every field: POST request to /api/issues/{project}", (done) => {
       done();
     });
+
     test("Create an issue with only required fields: POST request to /api/issues/{project}", (done) => {
       done();
     });
+
     test("Create an issue with missing required fields: POST request to /api/issues/{project}", (done) => {
       done();
     });
@@ -20,21 +35,54 @@ suite("Functional Tests", function () {
 
   suite("View issues", () => {
     test("View issues on a project: GET request to /api/issues/{project}", (done) => {
-      chai
-        .request(server)
-        .keepOpen()
+      request()
         .get("/api/issues/test")
         .end((err, res) => {
           assert.equal(res.status, 200);
           assert.isArray(res.body);
+          if (res.body.length > 0) {
+            const issue = res.body[0];
+            ISSUE_FIELDS.forEach((p) => assert.property(issue, p));
+          }
           done();
         });
     });
+
     test("View issues on a project with one filter: GET request to /api/issues/{project}", (done) => {
-      done();
+      request()
+        .get("/api/issues/test?open=true")
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          if (res.body.length > 0) {
+            const length = res.body.length > 100 ? 100 : res.body.length;
+            for (let i = 0; i < length; i++) {
+              const issue = res.body[i];
+              ISSUE_FIELDS.forEach((p) => assert.property(issue, p));
+              assert.isTrue(issue.open);
+            }
+          }
+          done();
+        });
     });
+
     test("View issues on a project with multiple filters: GET request to /api/issues/{project}", (done) => {
-      done();
+      request()
+        .get("/api/issues/test?open=true&assigned_to=Joe")
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          if (res.body.length > 0) {
+            const length = res.body.length > 100 ? 100 : res.body.length;
+            for (let i = 0; i < length; i++) {
+              const issue = res.body[i];
+              ISSUE_FIELDS.forEach((p) => assert.property(issue, p));
+              assert.isTrue(issue.open);
+              assert.strictEqual(issue.assigned_to, "Joe");
+            }
+          }
+          done();
+        });
     });
   });
 
@@ -42,15 +90,19 @@ suite("Functional Tests", function () {
     test("Update one field on an issue: PUT request to /api/issues/{project}", (done) => {
       done();
     });
+
     test("Update multiple fields on an issue: PUT request to /api/issues/{project}", (done) => {
       done();
     });
+
     test("Update an issue with missing _id: PUT request to /api/issues/{project}", (done) => {
       done();
     });
+
     test("Update an issue with no fields to update: PUT request to /api/issues/{project}", (done) => {
       done();
     });
+
     test("Update an issue with an invalid _id: PUT request to /api/issues/{project}", (done) => {
       done();
     });
@@ -60,9 +112,11 @@ suite("Functional Tests", function () {
     test("Delete an issue: DELETE request to /api/issues/{project}", (done) => {
       done();
     });
+
     test("Delete an issue with an invalid _id: DELETE request to /api/issues/{project}", (done) => {
       done();
     });
+
     test("Delete an issue with missing _id: DELETE request to /api/issues/{project}", (done) => {
       done();
     });
