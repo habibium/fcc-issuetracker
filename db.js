a87eb25bfd83c1f56a4bbd2896f0150ae0bec6b7
habibium <habib.prodev@gmail.com>
2024-10-19 21:45:36 +0600

    refactor: Update IssueSchema and add new functions

--- db.js
+++ db.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
-(async function connect() {
+const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.MURI);
     console.log("Successfully connected to MongoDB");
   } catch (error) {
     console.error("Failed to connect to MongoDB", error);
   }
-})();
+};
 
 const IssueSchema = mongoose.Schema({
-  assigned_to: String,
-  status_text: String,
+  project: { type: String, required: true },
+  assigned_to: { type: String, default: "" },
+  status_text: { type: String, default: "" },
   open: { type: Boolean, required: true, default: true },
   issue_title: { type: String, required: true },
   issue_text: { type: String, required: true },
@@ -19,3 +20,35 @@ const IssueSchema = mongoose.Schema({
   created_on: { type: Date, default: Date.now },
   updated_on: { type: Date, default: Date.now },
 });
+
+const Issue = mongoose.model("issue", IssueSchema);
+
+const createIssue = async (issue) => {
+  const newIssue = await Issue.create(issue);
+
+  return newIssue.toObject({
+    transform: (_doc, ret) => {
+      delete ret.project;
+      delete ret.__v;
+
+      return ret;
+    },
+  });
+};
+
+const findAllIssues = async (project, params) =>
+  await Issue.find({ project, ...params }).select("-project");
+
+const findIssueById = async (id) => await Issue.findById(id).exec();
+
+const updateIssue = async (id, updates) => {
+  const updatedIssue = await Issue.findByIdAndUpdate(id, updates).exec();
+  return updatedIssue;
+};
+
+module.exports = {
+  dbConnect,
+  createIssue,
+  findAllIssues,
+  findIssueById,
+};
